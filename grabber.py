86ae286ecf6ee6a6f5a334a7b97440eb687c395e
Viktor Varland <viktor@vardevs.se>
2015-03-20 17:20:59 +0100

    Add function to reconstruct a movie title from «Title, Prefix» to «Prefix Title»
    Side note. Unicode still trips me up. Gah.

--- grabber.py
+++ grabber.py
@@ -5,8 +5,23 @@ import codecs
 import sys
 import re
 
+import omdbapi
+
 from config import Config
-from omdbapi import search
+
+def reconstruct_title(t):
+    result = t
+    pattern = re.compile(ur'(?P<title>^.*)(?P<prefix>,\s\w+$)', re.U)
+    match = re.search(pattern, t)
+
+    if (match is not None):
+        prefix = match.group('prefix').lstrip(', ')
+        title = match.group('title')
+
+        result = u''+ prefix + ' ' + title
+
+    print u'Reconstructed title: {}'.format(result)
+    return result
 
 def movie_meta(dirname):
     pattern = re.compile(ur'(?P<title>^.*)\s\((?P<year>\d{4})\)', re.U)
@@ -16,7 +31,10 @@ def movie_meta(dirname):
     title = match.group('title')
     year = match.group('year')
     
-    movie = search(title=title, type='movie')
+    movie = omdbapi.search(title=reconstruct_title(title), type='movie')
+    print movie.get('Plot')
+    print movie.get('tomatoMeter')
+    print movie.get('imdbID')
 
     return dict(dirname=dirname, year=year, title=title)
 
@@ -30,20 +48,22 @@ def scan_tv(target):
     pass
 
 def scan_media(target):
-  print 'Scanning %s' % target
+  print u'Scanning {}'.format(target)
   dirlist = [d for d in os.listdir(target) if os.path.isdir(os.path.join(target, d))]
   return dirlist
   
 
 if __name__ == "__main__":
+    UTF8Writer = codecs.getwriter('utf8')
+    sys.stdout = UTF8Writer(sys.stdout)
 
-  cfg = Config()
-  paths = cfg.paths
-  media = {}
-        
-  for category in cfg.categories:
-    media[category] = []
-    dirs = scan_media(paths.get(category))
+    cfg = Config()
+    paths = cfg.paths
+    media = {}
+
+    for category in cfg.categories:
+        media[category] = []
+        dirs = scan_media(paths.get(category))
 
     if category == "music":
         scanner = scan_music
@@ -54,4 +74,3 @@ if __name__ == "__main__":
 
     for d in dirs:
         media[category].append(scanner(d))
-
--- omdbapi.py
+++ omdbapi.py
@@ -1,6 +1,7 @@
 import urllib
 import urllib2
 import json
+import re
 
 """
     t = title
@@ -13,10 +14,8 @@ import json
 url = 'http://www.omdbapi.com/?'
 
 def search(title='', imdbid='', type='', plot='short', r='json', tomatoes='true'):
-    data = dict(title=title.encode('utf-8'), imdbid=imdbid, type=type, plot=plot, r=r, tomatoes=tomatoes)
+    data = dict(t=title.encode('utf-8'), i=imdbid, type=type, plot=plot, r=r, tomatoes=tomatoes)
 
     req = urllib.urlencode(data)
-    print req
-    #resp = urllib2.urlopen(url).read()
-    #return json.loads(resp)
-
+    resp = urllib2.urlopen(url+req).read()
+    return json.loads(resp)
